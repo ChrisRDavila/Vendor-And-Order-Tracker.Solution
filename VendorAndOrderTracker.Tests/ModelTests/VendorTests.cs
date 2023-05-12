bae7e8e250d1fecc7bca6baf6568609157bc656b
ChrisRDavila <crossdavila@gmail.com>
2023-05-12 12:15:02 -0700

    Vendor GetAll List Test Passed

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorAndOrderTracker.Models;
 using System;
+using System.Collections.Generic;
 
 
 namespace VendorAndOrderTracker.Tests
@@ -76,6 +77,24 @@ namespace VendorAndOrderTracker.Tests
     int result = testVendor.VendorId;
 
     Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAllVendors_ReturnsAllVendorsObjects_VendorList()
+    {
+      string testName01 = "Cool Cafe";
+      string testDescribe01 = "small cart business";
+      string testAddress01 = "1234 Main";
+      string testName02 = "Bretts Bread";
+      string testDescribe02 = "Large Cafe";
+      string testAddress02 = "4321 First";
+      Vendor newVendor1 = new Vendor(testName01,testDescribe01,testAddress01);
+      Vendor newVendor2 = new Vendor(testName02, testDescribe02, testAddress02);
+      List<Vendor> newList = new List<Vendor> {newVendor1, newVendor2};
+
+      List<Vendor> result = Vendor.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }    
