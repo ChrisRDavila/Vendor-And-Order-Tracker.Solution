20e83b72fde03a128f5f985a3c12e9a9ffe41a7c
ChrisRDavila <crossdavila@gmail.com>
2023-05-12 10:40:59 -0700

    Get Vendor Properties Test passed, will get all vendor properties

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -12,21 +12,27 @@ namespace VendorAndOrderTracker.Tests
     public void VendorConstructor_CreatesVendorInstance_Vencor()
     {
       string testName = "Donnies";
-      string testDescribe = "Downtown Locaation";
+      string testDescribe = "Downtown Location";
       string testAddress = "2037 N First Ave";
       Vendor testVendor = new Vendor(testName, testDescribe, testAddress);
       Assert.AreEqual(typeof(Vendor), testVendor.GetType());
     }
 
-    // [TestMethod]
-    // public void GetProperties_GetsOrderProperties_StringAndInt()
-    // {
-    //   string testName = "Donnies";
-    //   string testDescribe = "Downtown Locaation";
-    //   string testAddress = "2037 N First Ave";
-    //   Vendor testVendor = new Vendor(testName, testDescribe, testAddress);
-    //   string resultName = 
-    //   Assert.AreEqual(typeof(Vendor), testVendor.GetType());
-    // }
+    [TestMethod]
+    public void GetVendorProperties_ReturnsVendorProperties_Strings()
+    {
+      string nameTest = "Donnies";
+      string describeTest = "Downtown Location";
+      string addressTest = "2037 N First Ave";
+      
+      Vendor testVendor = new Vendor(nameTest, describeTest, addressTest);
+      string resultName = testVendor.Name;
+      string resultDescribe = testVendor.Describe;
+      string resultAddress = testVendor.Address;
+      
+      Assert.AreEqual(nameTest, resultName);
+      Assert.AreEqual(describeTest, resultDescribe);
+      Assert.AreEqual(addressTest, resultAddress);
+    }
   }
 }    
