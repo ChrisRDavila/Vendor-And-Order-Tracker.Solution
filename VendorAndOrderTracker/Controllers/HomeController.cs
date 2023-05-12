d3ec0c1bb394cba18671fd3b68c5d5678e9c8348
ChrisRDavila <crossdavila@gmail.com>
2023-05-12 08:28:25 -0700

    change file names

--- VendorAndOrderTracker/Controllers/HomeController.cs
+++ VendorAndOrderTracker/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using ProjectName.Models;
+using VendorAndOrderTracker.Models;
 
-namespace ProjectName.Controllers
+namespace VendorAndOrderTracker.Controllers
 {
   public class HomeController : Controller
   {
