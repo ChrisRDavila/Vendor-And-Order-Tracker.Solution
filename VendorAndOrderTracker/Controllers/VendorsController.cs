df4cda226f9990558c14855ea3be3ac4bb51ded3
ChrisRDavila <crossdavila@gmail.com>
2023-05-12 14:28:22 -0700

    set up Show action to be connected with  Vendor Show Views in Vendors Controller

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -21,12 +21,25 @@ namespace VendorAndOrderTracker.Controllers
       return View();
     }
 
-    [HttpPost("/categories")]
+    [HttpPost("/vendors")]
     public ActionResult Create(string vendorName, string vendorDescribe, string vendorAddress)
     {
       Vendor newVendor = new Vendor(vendorName, vendorDescribe, vendorAddress);
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    
 
 
   }
