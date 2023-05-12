ce0042c3044bd9d4ab7d68b0510de2bb4e99c3d5
ChrisRDavila <crossdavila@gmail.com>
2023-05-12 14:41:51 -0700

    complete Vendor Controllers, need to set up corresponding Views

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -39,8 +39,19 @@ namespace VendorAndOrderTracker.Controllers
       return View(model);
     }
 
-    
-
-
+    // This one creates new Items within a given Category, not new Categories:
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
+  
   }
 }    
